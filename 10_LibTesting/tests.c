4da8985807d9cf7e022b3f72b754d8abcf8659b3
kirill3008 <arkhip0v0@qavm-f9b691f5.qemu>
2025-12-02 15:54:02 +0300

    HW10

---
+++ 10_LibTesting/tests.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "buf.h"
+
+/* Simple test framework */
+#if _WIN32
+#  define C_RED(s)     s
+#  define C_GREEN(s)   s
+#else
+#  define C_RED(s)     "\033[31;1m" s "\033[0m"
+#  define C_GREEN(s)   "\033[32;1m" s "\033[0m"
+#endif
+
+static int count_pass = 0;
+static int count_fail = 0;
+
+#define TEST(s, x) \
+    do { \
+        if (x) { \
+            printf(C_GREEN("PASS") " %s\n", s); \
+            count_pass++; \
+        } else { \
+            printf(C_RED("FAIL") " %s\n", s); \
+            count_fail++; \
+        } \
+    } while (0)
+
+static uint32_t
+pcg32(uint64_t *s)
+{
+    uint64_t m = 0x5851f42d4c957f2d;
+    uint64_t a = 0x8b260b70b8e98891;
+    uint64_t p = *s;
+    uint32_t x = ((p >> 18) ^ p) >> 27;
+    uint32_t r = p >> 59;
+    *s = p * m + a;
+    return (x >> r) | (x << (-r & 31u));
+}
+
+static unsigned long
+bench(uint64_t *rng)
+{
+    unsigned long r = 0;
+    uint32_t n = 1000000 + pcg32(rng) % 4000000;
+    float *buf = 0;
+
+    for (uint32_t i = 0; i < n; i++)
+        buf_push(buf, pcg32(rng) / (double)UINT32_MAX);
+
+    float threshold = pcg32(rng) / (double)UINT32_MAX;
+    for (uint32_t i = 0; i < n; i++)
+        r += buf[i] > threshold;
+
+    buf_free(buf);
+    return r;
+}
+
+#ifdef _WIN32
+#include <windows.h>
+uint64_t
+uepoch(void)
+{
+    FILETIME ft;
+    GetSystemTimeAsFileTime(&ft);
+    uint64_t tt = ft.dwHighDateTime;
+    tt <<= 32;
+    tt |= ft.dwLowDateTime;
+    tt /=10;
+    tt -= UINT64_C(11644473600000000);
+    return tt;
+}
+#else
+#include <sys/time.h>
+uint64_t
+uepoch(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return 1000000LL * tv.tv_sec + tv.tv_usec;
+}
+#endif
+
+/* Test initialization and free */
+static void test_init_free(void)
+{
+    float *a = 0;
+    TEST("capacity init", buf_capacity(a) == 0);
+    TEST("size init", buf_size(a) == 0);
+    buf_push(a, 1.3f);
+    TEST("size 1", buf_size(a) == 1);
+    TEST("value", a[0] == (float)1.3f);
+    buf_clear(a);
+    TEST("clear", buf_size(a) == 0);
+    TEST("clear not-free", a != 0);
+    buf_free(a);
+    TEST("free", a == 0);
+
+    /* Clearing a NULL pointer is a no-op */
+    buf_clear(a);
+    TEST("clear empty", buf_size(a) == 0);
+    TEST("clear no-op", a == 0);
+}
+
+/* Test push and access */
+static void test_push(void)
+{
+    long *ai = 0;
+    for (int i = 0; i < 10000; i++)
+        buf_push(ai, i);
+    TEST("size 10000", buf_size(ai) == 10000);
+    int match = 0;
+    for (int i = 0; i < (int)(buf_size(ai)); i++)
+        match += ai[i] == i;
+    TEST("match 10000", match == 10000);
+    buf_free(ai);
+}
+
+/* Test grow and trunc */
+static void test_grow_trunc(void)
+{
+    long *ai = 0;
+    buf_grow(ai, 1000);
+    TEST("grow 1000", buf_capacity(ai) == 1000);
+    TEST("size 0 (grow)", buf_size(ai) == 0);
+    buf_trunc(ai, 100);
+    TEST("trunc 100", buf_capacity(ai) == 100);
+    buf_free(ai);
+}
+
+/* Test pop */
+static void test_pop(void)
+{
+    float *a = 0;
+    buf_push(a, 1.1);
+    buf_push(a, 1.2);
+    buf_push(a, 1.3);
+    buf_push(a, 1.4);
+    TEST("size 4", buf_size(a) == 4);
+    TEST("pop 3", buf_pop(a) == (float)1.4f);
+    buf_trunc(a, 3);
+    TEST("size 3", buf_size(a) == 3);
+    TEST("pop 2", buf_pop(a) == (float)1.3f);
+    TEST("pop 1", buf_pop(a) == (float)1.2f);
+    TEST("pop 0", buf_pop(a) == (float)1.1f);
+    TEST("size 0 (pop)", buf_size(a) == 0);
+    buf_free(a);
+}
+
+int main(int argc, char **argv)
+{
+    /* Benchtest? */
+    if (argc > 1) {
+        uint64_t rng = strtoull(argv[1], 0, 16);
+        unsigned long r = 0;
+        uint64_t start = uepoch();
+        for (int i = 0; i < 300; i++)
+            r += bench(&rng);
+        double t = (uepoch() - start) / 1e6;
+        printf("%.6gs : actual %lu, expect 428859598\n", t, r);
+        return 0;
+    }
+
+    /* Run all tests */
+    test_init_free();
+    test_push();
+    test_grow_trunc();
+    test_pop();
+
+    /* Print summary */
+    printf("%d fail, %d pass\n", count_fail, count_pass);
+    return count_fail != 0;
+}
